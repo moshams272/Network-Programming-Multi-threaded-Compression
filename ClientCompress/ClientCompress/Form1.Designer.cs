eb5b90a279111843b06ed1c0fbbd63c5ff8770aa
Mohamed Shams El-Deen <68347231+moshams272@users.noreply.github.com>
2026-05-21 15:38:53 +0300

    Add files via upload

--- ClientCompress/ClientCompress/Form1.Designer.cs
+++ ClientCompress/ClientCompress/Form1.Designer.cs
@@ -32,21 +32,22 @@
             this.btnSelectFile = new System.Windows.Forms.Button();
             this.btnSend = new System.Windows.Forms.Button();
             this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtIP
             // 
-            this.txtIP.Location = new System.Drawing.Point(357, 105);
+            this.txtIP.Location = new System.Drawing.Point(242, 42);
             this.txtIP.Name = "txtIP";
-            this.txtIP.Size = new System.Drawing.Size(198, 24);
+            this.txtIP.Size = new System.Drawing.Size(226, 22);
             this.txtIP.TabIndex = 0;
             this.txtIP.Text = "127.0.0.1";
             // 
             // btnSelectFile
             // 
-            this.btnSelectFile.Location = new System.Drawing.Point(518, 214);
+            this.btnSelectFile.Location = new System.Drawing.Point(333, 179);
             this.btnSelectFile.Name = "btnSelectFile";
-            this.btnSelectFile.Size = new System.Drawing.Size(118, 70);
+            this.btnSelectFile.Size = new System.Drawing.Size(135, 37);
             this.btnSelectFile.TabIndex = 1;
             this.btnSelectFile.Text = "Select";
             this.btnSelectFile.UseVisualStyleBackColor = true;
@@ -54,9 +55,9 @@
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(264, 214);
+            this.btnSend.Location = new System.Drawing.Point(171, 179);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(99, 70);
+            this.btnSend.Size = new System.Drawing.Size(113, 37);
             this.btnSend.TabIndex = 2;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -64,17 +65,28 @@
             // 
             // lblStatus
             // 
-            this.lblStatus.Location = new System.Drawing.Point(222, 108);
+            this.lblStatus.Location = new System.Drawing.Point(168, 85);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(102, 39);
+            this.lblStatus.Size = new System.Drawing.Size(297, 79);
             this.lblStatus.TabIndex = 3;
-            this.lblStatus.Text = "label1";
+            this.lblStatus.Text = "Status: Not Connected";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP Address:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(653, 307);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.btnSelectFile);
@@ -92,6 +104,7 @@
         private System.Windows.Forms.Button btnSelectFile;
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
     }
 }
 
